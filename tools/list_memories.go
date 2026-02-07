45baa4a14b8b22f5610cab3887f65469df829c5a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-02-08 02:21:43 +0900

    Fixes tool signature

--- tools/list_memories.go
+++ tools/list_memories.go
@@ -4,13 +4,19 @@ import (
 	"hostapi"
 )
 
-var Name = "list_memories"
-var Description = "List all saved information. Returns a JSON object with all key-value pairs that have been remembered."
-var Parameters = `{
-	"type": "object",
-	"properties": {}
-}`
-
-func Run(args string) (string, error) {
-	return hostapi.ListMemory(), nil
+var Tool = struct {
+	Name        string
+	Description string
+	Parameters  string
+	Run         func(string) (string, error)
+}{
+	Name:        "list_memories",
+	Description: "List all saved information. Returns a JSON object with all key-value pairs that have been remembered.",
+	Parameters: `{
+		"type": "object",
+		"properties": {}
+	}`,
+	Run: func(args string) (string, error) {
+		return hostapi.ListMemory(), nil
+	},
 }
